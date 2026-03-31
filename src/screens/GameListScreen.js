e382e116d5effcc080cc18f75f7f028476c8a275
Sebastian <sergioisaac30@gmail.com>
2026-03-30 21:44:51 -0500

    In the GameListScreen.js file, code was implemented that allows us to view a list of games and includes buttons to navigate to the GameDetailScreen.js screen. This code uses useState, useEffect, and several React Native imports (FlatList, TouchableOpacity, Text, ActivityIndicator)

---
+++ src/screens/GameListScreen.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, TouchableOpacity, Text, ActivityIndicator } from "react-native";
+import { gamesData } from "../Data/gamesd";
+import styles from "../styles/GameListStyles";
+
+const GameListScreen = ({ navigation }) => {
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGames(gamesData);
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" style={styles.loader} />
+      ) : (
+        <FlatList
+          data={games}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.card}
+              onPress={() => navigation.navigate("GameDetail", { game: item })}
+            >
+              <Text style={styles.text}>
+                🎮 {item.title} - {item.platform} ({item.gender})
+              </Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default GameListScreen;
