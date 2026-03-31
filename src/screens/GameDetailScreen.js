d7a1c6c9f12335d147966e3d41769956f3236eb4
Sebastian <sergioisaac30@gmail.com>
2026-03-30 21:26:32 -0500

    In the GameDetailScreen.js file, code was written that allows us to view detailed information about a video game selected on the GameListScreen.js screen via navigation. This was done using useState and some React Native imports (Text, Button, View).

---
+++ src/screens/GameDetailScreen.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Text, Button, View } from "react-native";
+import styles from "../styles/GameDetailStyles";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const GameDetailScreen = ({ route, navigation }) => {
+  const { game } = route.params;
+  const [wishlist, setWishlist] = useState(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>{game.title}</Text>
+        <Text style={styles.platform}>{game.platform}</Text>
+        <Text style={styles.gender}>{game.gender}</Text>
+        <Text style={styles.price}>${game.price}</Text>
+        <Text style={styles.age}>{game.ageRating}</Text>
+        <Text style={styles.description}>{game.description}</Text>
+      </View>
+
+      <Button
+        title={wishlist ? "REMOVE" : "ADD"}
+        onPress={() => setWishlist(!wishlist)}
+      />
+      <Button title="BACK" onPress={() => navigation.goBack()} />
+    </SafeAreaView>
+  );
+};
+
+export default GameDetailScreen;
